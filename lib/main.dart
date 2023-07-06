ccb8022766bada0e830a4c976ee866ddfb60c02c
Miguel Edward Rosal <miguelrosal12@gmail.com>
2023-07-06 10:36:54 +0800

    SetState

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'NT Application',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Text and button'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String word = ' ';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Button'),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('This is a button'),
-          onPressed: () {},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Hello World'),
+              onPressed: () {
+                setState(() {
+                  word = 'Hello World';
+                });
+              },
+            ),
+            Text(
+              word,
+              style: const TextStyle(fontSize: 100),
+            ),
+          ],
         ),
       ),
     );
